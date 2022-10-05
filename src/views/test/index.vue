1d4f0099595813d7e1d8a9bc448ed4dc576301f6
xLong <381612175@qq.com>
2022-10-05 22:29:10 +0800

    修改

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="test-container">
-    <!-- 测试页面 -->
-    <div class="box">
-      <div class="top"></div>
-      <div class="bottom"></div>
-    </div>
+    测试内容
   </div>
 </template>
 
@@ -12,32 +8,4 @@
 </script>
 
 <style lang="scss" scoped>
-// .test-container {
-//   box-sizing: border-box;
-//   min-height: 100vh;
-//   padding: 20px;
-//   text-align: center;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   font-size: 20px;
-// }
-
-.box{
- display: flex;
-width: 100vw;
-height: 100vh;
-
-}
-
-.top{
- width: 300px;
-height: 100%;
-
-}
-
-.bottom{
-  flex:1;
-  height: 100%;;
-}
 </style>
